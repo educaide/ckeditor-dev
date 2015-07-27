ec52254de1f8bfbc19744723b2fcd5abbd987abc
Piotrek Koszuliński <pkoszulinski@gmail.com>
2015-07-27 17:17:00 +0200

    Created dedicated sample and config, aligned build config to them.

---
+++ easconfig.js
@@ -0,0 +1,50 @@
+﻿/**
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	config.plugins = [
+		'wysiwygarea',
+		'clipboard',
+		'undo',
+		'enterkey',
+		'toolbar',
+		'elementspath',
+		'contextmenu',
+		'htmlwriter',
+		'basicstyles',
+		'justify',
+		'list',
+		'indentlist',
+
+		'easbehaviors',
+		'easfontsize',
+		'easimage',
+		'easimagestyle',
+		'easintro',
+		'easliststyle',
+		'easmath',
+		'easmathimages',
+		'easparbox',
+		'easproperties',
+		'easspecials',
+		'eastable',
+		'eastabletoolscustom',
+		'easwordstyle'
+	].join( ',' );
+
+	config.toolbar = [
+		[ 'Undo', 'Redo' ],
+		[ 'Bold','Italic','Underline', '-', 'Subscript', 'Superscript' ],
+		[ 'EASWordStyle' ],
+		[ 'EASFontSize' ],
+		[ 'JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock', 'Indent', 'Outdent' ],
+		// [ 'EASList' ],
+		[ 'EASTable', 'EASImage', 'EASSpecials', '-', 'EASIntro', 'EASParBox', 'EASMathImages' ]
+	];
+
+	config.skin = 'kama';
+};
+
+// %LEAVE_UNMINIFIED% %REMOVE_LINE%
