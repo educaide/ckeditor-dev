7a6efe559bab2c3dc56f9e58c6c3528be94ae6df
Piotrek Koszuliński <pkoszulinski@gmail.com>
2015-07-28 16:40:10 +0200

    Updated code of easliststyle and fixed a bug when if <li> contains a block, marking it as a correct answer was not possible.

--- plugins/easliststyle/plugin.js
+++ plugins/easliststyle/plugin.js
@@ -1,9 +1,6 @@
-/*
-Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
+'use strict';
 
-(function() {
+( function() {
 
   function listCommand( name ) {
     this.name = name;
@@ -14,30 +11,31 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
       editor.focus();
 
       var doc = editor.document,
-      selection = editor.getSelection(),
-      ranges = selection && selection.getRanges( true );
+        selection = editor.getSelection(),
+        ranges = selection && selection.getRanges();
 
       // There should be at least one selected range.
       if ( !ranges || ranges.length < 1 )
         return;
 
-      // find root OL
-      var liElem = selection.getStartElement();
-      var olElem = new CKEDITOR.dom.element( liElem.$.parentNode );
-      if ( liElem.$.tagName != "LI" || olElem.$.tagName != "OL" )
+      // Find root OL.
+      var liElem = selection.getStartElement().getAscendant( 'li', true );
+      var olElem = liElem.getParent();
+
+      if ( !liElem || !olElem.is( 'ol' ) )
         return;
 
       if ( this.name == 'markcorrectanswer' ) {
-        // clear any existing 'correct' markers
-        for ( var i = 0; i < olElem.$.childNodes.length; i++ ) {
-          new CKEDITOR.dom.element( olElem.$.childNodes[i] ).removeClass( 'correct' );
+        // Clear any existing 'correct' markers
+        for ( var i = 0; i < olElem.getChildCount(); i++ ) {
+          olElem.getChild( i ).removeClass( 'correct' );
         }
 
-        // mark selected LI as correct
+        // Mark selected LI as correct.
         liElem.addClass( 'correct' );
       }
       else if ( this.name == 'listproperties' ) {
-        alert('to be implemented');
+        alert( 'to be implemented' );
       }
     }
   };
@@ -69,31 +67,33 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 
       // If the "contextmenu" plugin is loaded, register the listeners.
       if ( editor.contextMenu ) {
-	editor.contextMenu.addListener( function( element, selection ) {
+        editor.contextMenu.addListener( function( element ) {
           if ( !element || element.isReadOnly() )
             return null;
 
-          // TODO consider requiring Prototype and using its helpers to do this kind of stuff
-          // find parent OL of element
-          while ( element ) {
-            var name = element.getName();
-            if ( name == 'ol' ) {
-              // TODO cross-browser way to detect this
-              var isMultiItemSelection = false;
-              if ( element.hasClass( 'emcee' ) )
-                return { markcorrectanswer: isMultiItemSelection ? CKEDITOR.TRISTATE_DISABLED : CKEDITOR.TRISTATE_OFF,
-                         listproperties: CKEDITOR.TRISTATE_OFF };
-              else if ( !element.hasClass( 'emcee' ) )
-                return { listproperties: CKEDITOR.TRISTATE_OFF };
-            }
+          var list = element.getAscendant( 'ol', true );
 
-            element = element.getParent();
+          if ( !list ) {
+            return null;
           }
-          return null;
-        });
+
+          // TODO
+          var isMultiItemSelection = false;
+
+          if ( list.hasClass( 'emcee' ) ) {
+            return {
+              markcorrectanswer: isMultiItemSelection ? CKEDITOR.TRISTATE_DISABLED : CKEDITOR.TRISTATE_OFF,
+              listproperties: CKEDITOR.TRISTATE_OFF
+            };
+          } else if ( !element.hasClass( 'emcee' ) ) {
+            return {
+              listproperties: CKEDITOR.TRISTATE_OFF
+            };
+          }
+        } );
       }
     }
   };
 
   CKEDITOR.plugins.add( 'easliststyle', CKEDITOR.plugins.easliststyle );
-})();
+} )();
