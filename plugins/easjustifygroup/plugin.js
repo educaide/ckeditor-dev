7b567724030f4daff512e5ffd9f681e1f55e5082
Patrick Davey <patrick.davey@gmail.com>
2019-01-29 14:48:48 +1300

    wip

--- plugins/easjustifygroup/plugin.js
+++ plugins/easjustifygroup/plugin.js
@@ -1,16 +1,45 @@
 (function() {
   'use strict';
-  var COMMANDS = ['justifycenter', 'justifyright', 'justifyblock'];
+  var COMMANDS = ['justifyleft', 'justifycenter', 'justifyright', 'justifyblock'];
 
   function onSelectionChange(event) {
     if (event.editor.readOnly) {
       return;
     }
+    // turn them all off.
     _.each(COMMANDS, function(command) {
       var c = event.editor.getCommand(command);
       c.setState(CKEDITOR.TRISTATE_OFF)
-
     })
+
+    var pathElements = event.data.path.elements;
+    var i;
+    for (i = 0; i < pathElements.length; i++) {
+      var element = pathElements[i].$;
+      if (element.nodeName === "P") {
+        if (element.style.getPropertyValue('text-align') === "center") {
+          var command = event.editor.getCommand('justifycenter');
+          command.setState(CKEDITOR.TRISTATE_ON)
+          var command = event.editor.getCommand('justifyleft');
+          command.setState(CKEDITOR.TRISTATE_ON)
+          return
+        } else if (element.style.getPropertyValue('text-align') === "right") {
+          var command = event.editor.getCommand('justifyright');
+          command.setState(CKEDITOR.TRISTATE_ON)
+          var command = event.editor.getCommand('justifyleft');
+          command.setState(CKEDITOR.TRISTATE_ON)
+          return
+        } else if (element.style.getPropertyValue('text-align') === "justify") {
+          var command = event.editor.getCommand('justifyblock');
+          command.setState(CKEDITOR.TRISTATE_ON)
+          var command = event.editor.getCommand('justifyleft');
+          command.setState(CKEDITOR.TRISTATE_ON)
+          return
+        }
+      }
+      var command = event.editor.getCommand('justifyleft');
+      command.setState(CKEDITOR.TRISTATE_OFF)
+    }
     // debugger
     // command.setState(CKEDITOR.TRISTATE_ON);
     // if (event.editor.readOnly) {
@@ -35,6 +64,7 @@
   }
 
   CKEDITOR.plugins.add('easjustifygroup', {
+    icons: 'wibble',
     requires: ['justify'],
     init: function (editor) {
       var items = {
@@ -70,18 +100,20 @@
 
       editor.ui.add('JustifyGroup', CKEDITOR.UI_MENUBUTTON, {
         label: 'Justify Group',
-        icon: 'JustifyLeft',
+        icon: 'wibble',
         // Disable in source mode.
         modes: {
           wysiwyg: 1
         },
         onRender: function() {
+          console.log("in onrender")
           var command = editor.getCommand('justifyleft');
           command.on('state', function() {
             this.setState(command.state);
           }, this);
         },
         onMenu: function () {
+          console.log("in onmenu")
           var activeItems = {};
 
           // Make all items active.
